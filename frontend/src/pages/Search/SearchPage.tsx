a182f64b3544fabee08f5fe4f6b679dfd5351a8a
Eymard Neto <t_francisco.gomes@hapvida.com.br>
2025-04-23 22:06:06 -0300

    add searchbar

--- frontend/src/pages/Search/SearchPage.tsx
+++ frontend/src/pages/Search/SearchPage.tsx
@@ -1,14 +1,48 @@
-import { Input } from '@mui/material';
+import { InputAdornment, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import './style.css'
 
 export default function SearchPage() {
   return (
     <>
-      <div className=''>
+      <div className='search-container'>
         <div className='top-bar'>
-          <div className="welcome-text">
-            <Input about='search' placeholder='Pesquisar...' className='search-input' />
-          </div>
+          <TextField
+            fullWidth
+            placeholder="Pesquisar..."
+            variant="outlined"
+            size="small"
+            slotProps={{
+              input: {
+                sx: { 
+                 color: '#EBE4C1'
+                },
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }
+            }}
+            sx={{
+              m: 1,
+              mb: 2,
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#EBE4C1',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#EBE4C1',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#EBE4C1',
+                },
+              },
+              '& .MuiInputAdornment-root': {
+                color: '#EBE4C1',
+              },
+            }}
+          />
         </div>
       </div>
     </>
